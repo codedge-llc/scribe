943dff5189253ed4c466434cc4c4560b0037dd69
Henry Popp <henry@codedge.io>
2017-03-27 09:04:36 -0500

    feat: default styling no longer separates data rows

--- lib/style/default.ex
+++ lib/style/default.ex
@@ -6,8 +6,19 @@ defmodule Scribe.Style.Default do
 
   @behaviour Scribe.Style
 
-  def border_at(_row, _columns, _max_rows, _max_cols) do
+  def border_at(0, _columns, _max_rows, _max_cols) do
     Border.new("+", "|", "-")
+  end
+
+  def border_at(row, _col, max_rows, _max_cols) when row == max_rows - 1 do
+    Border.new("+", "|", "-")
+  end
+
+  def border_at(_row, _col, _max_rows, _max_cols) do
+    %Border{
+      left_edge: "|",
+      right_edge: "|"
+    }
   end
 
   use Scribe.DefaultColors
