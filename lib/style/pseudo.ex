083d3e020c3cce16279823c3acced8cb46e76371
Aleksei Matiushkin <am@mudasobwa.ru>
2018-02-19 21:21:49 +0100

    Fixed inner rows border.

--- lib/style/pseudo.ex
+++ lib/style/pseudo.ex
@@ -3,7 +3,18 @@ defmodule Scribe.Style.Pseudo do
 
   @behaviour Scribe.Style
 
-  @default_border %Scribe.Border{
+  @empty_border %Scribe.Border{
+    bottom_edge: "",
+    bottom_left_corner: "",
+    bottom_right_corner: "",
+    top_edge: "",
+    top_left_corner: "",
+    top_right_corner: "",
+    left_edge: "│",
+    right_edge: "│"
+  }
+
+  @header_border %Scribe.Border{
     bottom_edge: "─",
     bottom_left_corner: "└",
     bottom_right_corner: "┘",
@@ -14,40 +25,32 @@ defmodule Scribe.Style.Pseudo do
     right_edge: "│"
   }
 
-  def border_at(row, col, max_rows, max_cols)
-      when row > 0 and col > 0 and row < max_rows - 1 and col < max_cols - 1 do
-    %Scribe.Border{@default_border |
-      top_left_corner: "┼", top_right_corner: "┼",
-      bottom_left_corner: "┼", bottom_right_corner: "┼"
+  def border_at(0, col, _max_rows, max_cols) when col < max_cols - 1 do
+    %Scribe.Border{@header_border |
+      bottom_left_corner: "├", bottom_right_corner: "┼", top_right_corner: "┬"
     }
   end
 
-  def border_at(0, 0, _max_rows, 1) do
-    %Scribe.Border{@default_border |
-      bottom_left_corner: "├", bottom_right_corner: "┤"}
-  end
-
-  def border_at(0, col, _max_rows, max_cols) when col < max_cols - 1 do
-    %Scribe.Border{@default_border |
-      top_right_corner: "┬",
-      bottom_left_corner: "├", bottom_right_corner: "┼"}
-  end
-
-  def border_at(row, col, max_rows, max_cols) when col == max_cols - 1 and row < max_rows - 1 do
-    %Scribe.Border{@default_border | bottom_right_corner: "┤"}
+  def border_at(0, _col, _max_rows, _max_cols) do
+    %Scribe.Border{@header_border |
+      bottom_left_corner: "├", bottom_right_corner: "┤"
+    }
   end
 
   def border_at(row, col, max_rows, max_cols) when row == max_rows - 1 and col < max_cols - 1 do
-    %Scribe.Border{@default_border | bottom_right_corner: "┴"}
+    %Scribe.Border{@header_border |
+      top_left_corner: "", top_right_corner: "", bottom_right_corner: "┴"
+    }
   end
 
-  def border_at(row, 0, max_rows, _max_cols) when row < max_rows - 1 do
-    %Scribe.Border{@default_border |
-      bottom_left_corner: "├", bottom_right_corner: "┼"}
+  def border_at(row, _col, max_rows, _max_cols) when row == max_rows - 1 do
+    %Scribe.Border{@header_border |
+      top_left_corner: "", top_right_corner: ""
+    }
   end
 
   def border_at(_row, _col, _max_rows, _max_cols) do
-    @default_border
+    @empty_border
   end
 
   use Scribe.DefaultColors
